26ecf5c39c901748c83ba352974fc08c2cc019c3
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-01-07 18:24:36 -0800

    Quick fix

--- StopWatchPrototype/ViewController.swift
+++ StopWatchPrototype/ViewController.swift
@@ -9,18 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    
+    var time = 0;
+    
 
     @IBOutlet weak var timerLabel: UILabel!
     
     
+    func increaseTimer() {
+        time++;
+        timerLabel.text = String(time);
+    }
+    
+    
     @IBAction func play(sender: AnyObject) {
+        
+       timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
     }
     
     @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate();
     }
     
     
     @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate();
+        time = 0;
+        timerLabel.text = "0";
     }
     
     override func viewDidLoad() {
