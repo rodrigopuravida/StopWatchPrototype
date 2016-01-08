921b6a5fa4c761321c1c19c282ab92af4a74ff2f
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-01-07 17:55:39 -0800

    Initial commit

--- StopWatchPrototype/ViewController.swift
+++ StopWatchPrototype/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    
+    @IBAction func play(sender: AnyObject) {
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
